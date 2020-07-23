66396f3ab27284b35e6d01799070b61c06650210
buggishop <buggishoponline@gmail.com>
2020-07-23 11:33:50 +0530

    settings

--- buggiproj/buggiproj/settings.py
+++ buggiproj/buggiproj/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'buggiapp',
+
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,10 @@ WSGI_APPLICATION = 'buggiproj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME':'Master','Orderdb','Transaction','Comm_service',
+        'USER':'root',
+        'PASSWORD':'windows',
+        'HOST':'localhost',
     }
 }
 
